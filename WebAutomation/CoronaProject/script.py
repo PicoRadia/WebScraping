a585684e67fc677f64d6a908f5c65b5d8e5d99d4
PicoRadia <53488041+PicoRadia@users.noreply.github.com>
2020-05-13 14:56:28 +0000

    Update script.py

--- WebAutomation/CoronaProject/script.py
+++ WebAutomation/CoronaProject/script.py
@@ -9,3 +9,21 @@ sys.append(vendor_dir)
 
 import requests
 import bs4
+
+''' Fast Dev '''
+#https://www.worldometers.info/coronavirus/
+
+from bs4 import BeautifulSoup as bs4
+import requests
+import os
+import time
+
+#collecting the html document
+response = requests.get("https://www.worldometers.info/coronavirus/")
+
+#making the soup 
+soup = bs(response, "html.parser")
+head = soup.select("#main_table_countries_today > thead")
+print(head)
+
+                    
