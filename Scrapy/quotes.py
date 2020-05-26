1d679f2bdc1e1d6283bb436ecb67c992a480db9d
PicoRadia <53488041+PicoRadia@users.noreply.github.com>
2020-05-26 20:30:17 +0100

    scraping quotes
    url = "https://quotes.toscrape.com"

---
+++ Scrapy/quotes.py
@@ -0,0 +1,49 @@
+#	* * * * * * * * * * * * * * * * * * * * * * *
+#	*											*
+#	*	Scraping quotes with scrapy      		*
+#	*											*
+#	*				 By							*
+#	* 											*
+#	*			Fast   Dev						*
+#	*											*	
+#	*						May 26,2020			*
+#	*											*
+#	* * * * * * * * * * * * * * * * * * * * * * *
+
+import scrapy
+from scrapy.crawler import CrawlerProcess
+import json
+
+class QuotesScraper(scrapy.Spider):
+	name = 'quotes'
+	start_urls = ['https://quotes.toscrape.com']
+	
+	def parse(self,response) :
+		# casual way
+		quotes = response.css('span.text::text').getall() # to return a list
+		authors = response.css('small.author::text').getall()	
+		for index in range(len(authors)) : 
+			item = {
+						'author' : authors[index],
+						'quotes' : quotes[index]	
+					}
+			#yield item  ::  it's cool lol
+			result = json.dumps(item,indent = 2) # to pretty print the response
+			#print(result)
+			
+			
+		# pythonic way
+		for quote in response.css('div.quote'):
+			yield {
+				'quote' : quote.css('span.text::text').get(),
+				'author': quote.css('small.author::text').get()
+
+			}
+
+
+
+#run spider
+
+process = CrawlerProcess()
+process.crawl(QuotesScraper)
+process.start()
