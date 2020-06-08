54df31fdf49b4f960f298fe1bc522d35f5471188
PicoRadia <53488041+PicoRadia@users.noreply.github.com>
2020-06-08 04:29:15 +0100

    Better version

--- requests/PDFScraper.py
+++ requests/PDFScraper.py
@@ -32,31 +32,35 @@ urls = html.find_all('a')
 # starting
 print('**************************Starting*****************************')
 # loop over urls to extract pdf urls only 
-
+pdf =[]
 	# base url
 base_url = 'https://www.dansguitar.com'
 for url in urls :
 	try :
-		# count 
-		i=1
 		# get only pdf urls
 		if '.pdf' in url['href']:
 			# make get request to download the pdfs
 			link = str(base_url+url['href'])
-			print(link)
-			pdfs_url = requests.get(link)
-			print("Get request done \n")
-			# write pdf to local file
-			filename = str("./Licks/lick%s" %i)
-			print(filename)
-			with open(filename ,'wb') as f :
-				f.write(pdfs_url.content)
-			print('file %s has been successfully downloaded \n' %i)		
-			i+=1
+			pdf.append(link)
 	
 	# exclude all other urls
 	except Exception as e:
 		print(e)
+
+# count 
+i=1
+for p in pdf:
+	pdfs_url = requests.get(p)
+	# write pdf to local file
+	filename = str("./Licks/lick%s" %i)
+	print(filename)
+	with open(filename ,'wb') as f :
+		f.write(pdfs_url.content)
+
+	print("Get request done \n")		
+	print('file %s has been successfully downloaded \n' %i)		
+	i+=1
+
 
 
 
